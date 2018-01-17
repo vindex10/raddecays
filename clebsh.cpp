2f5f35346741559d4bc4268ab9866522da46e66f
Victor Ananyev <vindex10@gmail.com>
2018-01-17 18:26:34 +0200

    add condon-shortley phase factor

--- clebsh.cpp
+++ clebsh.cpp
@@ -7,5 +7,5 @@ using namespace std::complex_literals;
 using std::complex;
 
 complex<double> sphHarm(int l, int m, double theta, double phi) {
-    return gsl_sf_legendre_sphPlm(l, (m<0?-m:m), cos(theta))*exp(1i*(double)m*phi);
+    return (m%2 ? -1:1)*gsl_sf_legendre_sphPlm(l, (m<0?-m:m), cos(theta))*exp(1i*(double)m*phi);
 }

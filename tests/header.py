0f6b5786f18ff506fbaf8088191f282812be715e
Victor Ananyev <vindex10@gmail.com>
2019-05-19 19:31:08 +0300

    make axes labelsize BIGGER. remove LSR0 from article results and plots

--- tests/header.py
+++ tests/header.py
@@ -31,7 +31,7 @@ FIG_VSIZE = 6
 
 plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
 plt.rc('axes', titlesize=MEDIUM_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
 plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
